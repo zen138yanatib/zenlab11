724517d906e67405f3394da97157c33e2804c040
zen138yanatib <64641188+zen138yanatib@users.noreply.github.com>
2021-01-15 16:07:23 +0700

    Update lab11_1.cpp

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 using namespace std;
+long long int fibonacci(int );
 
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+long long int fibonacci(int x){
+    if(x<=1){
+        return x;
+    }else{
+        return fibonacci(x-1)+fibonacci(x-2);
+    }
 }
