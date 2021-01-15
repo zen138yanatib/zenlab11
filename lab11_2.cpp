7db5b0a05966408aa14e11624bc90177f63b2468
zen138yanatib <64641188+zen138yanatib@users.noreply.github.com>
2021-01-15 16:08:14 +0700

    Update lab11_2.cpp

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -1,9 +1,21 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int x,int y);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+int gcd(int x,int y){
+    int ans;
+    if(x%y == 0){
+        ans = y;
+        return ans;
+    }else{
+        ans = gcd(y,x%y);
+        return ans;
+    }
 }
